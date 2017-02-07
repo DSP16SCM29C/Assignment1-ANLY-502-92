e05405aa0461f643a6742c903996f7208b0a96bd
seungyeon <seungyeonlee01@gmail.com>
2017-02-07 16:02:42 -0600

    Update Scatterplot-homework.R

--- Scatterplot-homework.R
+++ Scatterplot-homework.R
@@ -1,4 +1,12 @@
-> setwd("/Users/almmotamedi/Desktop/R")
-> mytable=read.csv("/Users/almmotamedi/Desktop/R/Workbook_Example_Bivariate_dataset.csv", header=TRUE)
+> setwd("C:/Users/Seungyeon_L/Desktop/Seungyeon_RFolder")
+> mytable=read.csv("C:/Users/Seungyeon_L/Desktop/Seungyeon_RFolder/Workbook_Example_Bivariate_dataset.csv", header=TRUE)
 mytable
-plot(mytable$Profit, mytable$Number.of.Defective.Items)
+plot(mytable$Profit, mytable$Number.of.Defective.Items,
+     main="Profit and Defective Item Scatter Plot",#title name
+     xlab="Proffit in Thousands",#x-axis name
+     ylab="Number of Defective Items",#y-axis name
+     frame.plot=false,#yes, put line
+     pch=1, #circle shape
+     cex=1, #defalt size
+    col="red" #red
+    )
